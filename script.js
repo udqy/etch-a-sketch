08b91a083dc9f6f876fe14ff0277002ad428f662
uday <udaygitlog@gmail.com>
2022-09-10 10:27:14 +0530

    rework grid with js

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-//variables
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+const grid = document.querySelector('.grid')
 
-
-defaultGrid();
-//Creates a default grid sized 16x16 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-//Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    //Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
+createGrid = () => {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div); 
+    }
 };
 
-//Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+createGrid();
